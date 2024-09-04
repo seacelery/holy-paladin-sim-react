50f3a46253a16c256394c9cb32bae0667125aaf6
Amelia <seaceleries@gmail.com>
2024-09-04 22:15:29 +0100

    update config.js

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,9 +1,7 @@
 const isDevelopment = window.location.hostname === "localhost";
 
 const CONFIG = {
-    backendUrl: isDevelopment 
-        ? "http://localhost:5000" 
-        : 'https://holy-paladin-sim-react-5296562cb014.herokuapp.com'
+    backendUrl: 'https://holy-paladin-sim-react-5296562cb014.herokuapp.com'
 };
 
 export { CONFIG };

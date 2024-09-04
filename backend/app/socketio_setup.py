38b2a43c9d060a332f7daeea88f812fe0b67c1b0
Amelia <seaceleries@gmail.com>
2024-09-04 21:48:28 +0100

    update socketio setup

--- backend/app/socketio_setup.py
+++ backend/app/socketio_setup.py
@@ -1,3 +1,6 @@
 from flask_socketio import SocketIO
 
-socketio = SocketIO(cors_allowed_origins=["http://localhost:3000"])
+socketio = SocketIO(cors_allowed_origins="*")
+
+def init_socketio(app):
+    socketio.init_app(app, async_mode='eventlet', logger=True, engineio_logger=True)

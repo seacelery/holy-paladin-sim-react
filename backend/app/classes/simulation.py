e36ea60cbf22442ba3d904869bfe02d4d1a05bb7
Amelia <seaceleries@gmail.com>
2024-10-16 01:59:42 +0100

    log light of the martyr uptime

--- backend/app/classes/simulation.py
+++ backend/app/classes/simulation.py
@@ -55,6 +55,7 @@ class Simulation:
         self.iced_phial_timer = 0
         self.saving_graces_timer = 0
         self.light_of_the_martyr_uptime = float(light_of_the_martyr_uptime)
+        print(f"light of the martyr uptime: {self.light_of_the_martyr_uptime}")
         self.light_of_the_martyr_timer = 0
         self.bestow_light_timer = 0
         self.retribution_aura_timer = 40
